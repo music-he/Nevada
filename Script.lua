0fe5ce38f8b56e75b34ea3b461b382a8a0555073
music-he <nguyenphuoc20007@gmail.com>
2026-04-06 23:26:55 +0700

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,18 +1,4 @@
-if game.SoundService:FindFirstChild("MyMusic") then
-    game.SoundService.MyMusic:Destroy()
-end
-
-local sound = Instance.new("Sound")
-sound.Name = "MyMusic"
-sound.SoundId = "rbxassetid://85204826212798"
-sound.Volume = 3
-sound.Looped = true
-sound.Parent = game.SoundService
-
-sound.Loaded:Wait()
-
-if sound.TimeLength > 0 then
-    sound:Play()
-else
-    warn("Nhạc lỗi hoặc bị 0:00!")
-end
+local s = Instance.new("Sound")
+s.SoundId = "rbxassetid://81462736220763"
+s.Parent = game.SoundService
+s:Play()
